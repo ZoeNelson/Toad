482c39d4f1183b899f796d1fbca750686921e3ab
ZoeNelson <63169027+ZoeNelson@users.noreply.github.com>
2021-03-17 14:18:28 -0800

    Cookie clicker

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('type "node app.js" to see this message printed out!')
+
+function count() {
+    counter = Document.getElementById('counter')
+    counter.value += 1
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('is it working')
+
+function count() {
+    counter = document.getElementById('counter')
+    counter.innerHTML = 1 + Number(counter.innerHTML)
+}
